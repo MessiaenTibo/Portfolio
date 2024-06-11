b46ecf489a2e978233f1882a313d487bcf0754de
TiboMessiaen <tibo.messiaen@student.howest.be>
2024-06-11 19:06:43 +0200

    Feat: about me fade in animations

--- Portfolio/src/components/Aboutme.tsx
+++ Portfolio/src/components/Aboutme.tsx
@@ -14,7 +14,7 @@ export default () => {
     <div className='my-4 p-10 bg-white shadow-md dark:bg-dark lg:border-l-8 border-caribbean-current dark:border-caribbean-current-dark lg:m-12 2xl:max-w-8xl 2xl:mx-auto overflow-hidden relative'>
       <div className='flex flex-col lg:flex-row items-center xl-items-start'>
         <div className='lg:w-1/2'>
-          <div className=''>
+          <div className=' motion-safe:animate-fade-in-medium'>
             <h1 className='text-3xl font-bold mb-6 text-caribbean-current dark:text-caribbean-current-dark 2xl:text-4xl'>About me</h1>
             <h2 className='text-xl font-bold mb-4 2xl:text-2xl'>Hello, I'm Tibo</h2>
             <p className='mb-6 2xl:text-xl'>I always liked the idea of being able to code my own app's and possible solutions for many problems. I can always program my app's exactly the way I want it to be, and you never have to pay any subscription fees.</p>
@@ -25,27 +25,27 @@ export default () => {
           <div className=''>
             <h2 className='text-xl font-bold mb-4'>My hobbies</h2>
             <div className='flex flex-wrap'>
-              <div className='flex items-center'>
+              <div className='flex items-center motion-safe:animate-fade-in-from-left-1'>
                 <Code2 className='stroke-orange-peel dark:stroke-orange-peel-dark rounded-full bg-anti-flash-white dark:bg-anti-flash-white-dark p-2  m-2 xsm:h-16 xsm:w-16' size={50} />
                 <h2 className=' pl-2 pr-4 xsm:text-xl'>Code</h2>
               </div>
-              <div className='flex items-center'>
+              <div className='flex items-center motion-safe:animate-fade-in-from-left-2'>
                 <Waves className='stroke-orange-peel dark:stroke-orange-peel-dark rounded-full bg-anti-flash-white dark:bg-anti-flash-white-dark p-2  m-2 xsm:h-16 xsm:w-16' size={50} />
                 <h2 className=' pl-2 pr-4 xsm:text-xl'>Swimming</h2>
               </div>
-              <div className='flex items-center'>
-                  <Target className='stroke-orange-peel dark:stroke-orange-peel-dark rounded-full bg-anti-flash-white dark:bg-anti-flash-white-dark p-2  m-2 xsm:h-16 xsm:w-16' size={50} />
-                  <h2 className=' pl-2 pr-4 xsm:text-xl'>Darts</h2>
+              <div className='flex items-center motion-safe:animate-fade-in-from-left-3'>
+                <Target className='stroke-orange-peel dark:stroke-orange-peel-dark rounded-full bg-anti-flash-white dark:bg-anti-flash-white-dark p-2  m-2 xsm:h-16 xsm:w-16' size={50} />
+                <h2 className=' pl-2 pr-4 xsm:text-xl'>Darts</h2>
               </div>
-              <div className='flex items-center'>
-                  <Gamepad2 className='stroke-orange-peel dark:stroke-orange-peel-dark rounded-full bg-anti-flash-white dark:bg-anti-flash-white-dark p-2  m-2 xsm:h-16 xsm:w-16' size={50} />
-                  <h2 className=' pl-2 pr-4 xsm:text-xl'>Gaming</h2>
+              <div className='flex items-center motion-safe:animate-fade-in-from-left-4'>
+                <Gamepad2 className='stroke-orange-peel dark:stroke-orange-peel-dark rounded-full bg-anti-flash-white dark:bg-anti-flash-white-dark p-2  m-2 xsm:h-16 xsm:w-16' size={50} />
+                <h2 className=' pl-2 pr-4 xsm:text-xl'>Gaming</h2>
               </div>
             </div>
           </div>
         </div>
 
-        <div className='lg:w-1/2 lg:p-4 w-full'>
+        <div className='lg:w-1/2 lg:p-4 w-full motion-safe:animate-fade-in-medium'>
           {/* <CloudinaryImage link='AboutMe_rhhb8j.png' alt='AboutMe' imgClass='rounded-3xl shadow-xl w-full'/> */}
           <ImageCarousel />
         </div>
