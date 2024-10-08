4fd2e5c63b6dd89abdbf8c0281140659f4640879
TiboMessiaen <tibo.messiaen@student.howest.be>
2024-10-08 23:59:31 +0200

    Update: renew technologies project page

--- Portfolio/src/components/ProjectInfo.tsx
+++ Portfolio/src/components/ProjectInfo.tsx
@@ -11,22 +11,107 @@ interface Props {
     footnoteLinkTwo?: ReactElement<HTMLAttributeAnchorTarget>;
 }
 
+
+const backgroundColorsForEachTechnologie = [
+    {
+        tech: 'HTML',
+        color: '#e65027'
+    },
+    {
+        tech: 'CSS',
+        color: '#0c73b8'
+    },
+    {
+        tech: 'Javascript',
+        color: '#e7a328'
+    },
+    {
+        tech: 'Tailwindcss',
+        color: '#0c73b8'
+    },
+    {
+        tech: 'React',
+        color: '#23272f'
+    },
+    {
+        tech: 'ReactNative',
+        color: '#23272f'
+    },
+    {
+        tech: 'C#',
+        color: '#a179dc'
+    },
+    {
+        tech: 'C',
+        color: '#659ad2'
+    },
+    {
+        tech: 'Azure',
+        color: '#0078d7'
+    },
+    {
+        tech: 'WebApp',
+        color: '#0078d7'
+    },
+    {
+        tech: 'KeyVault',
+        color: '#0078d7'
+    },
+    {
+        tech: 'BlobStorage',
+        color: '#0078d7'
+    },
+    {
+        tech: 'CosmosDB',
+        color: '#0078d7'
+    },
+    {
+        tech: 'Python',
+        color: '#3670a0'
+    },
+    {
+        tech: 'MySQL',
+        color: '#00618b'
+    },
+    {
+        tech: 'Socket IO',
+        color: '#0F0F0F'
+    },
+    {
+        tech: 'NodeJS',
+        color: '#8ac500'
+    }
+]
+
 export default ({ title, paragraph, technologies, footnote, footnoteLink, footnoteTwo, footnoteLinkTwo }: Props) => {
+    const getBackgroundByTech = (tech: string) => {
+        const backgroundColor = backgroundColorsForEachTechnologie.find(e => e.tech.toLowerCase().split(' ').join('') === tech.toLocaleLowerCase().split(' ').join(''));
+        return backgroundColor ? backgroundColor.color : null; // Return null if no specific color is found
+    }
+
     return (
         <div>
             <h1 className='text-2xl font-bold mb-4'>{title}</h1>
             <p className='text-xl font-regular pb-2'>{paragraph}</p>
             <div className='mb-6 bg-white dark:bg-dark border-l-6 border-caribbean-current dark:border-caribbean-current-dark max-w-xl'>
-                <h1 className="text-lg p-6">Technologies</h1>
+                <h1 className="text-lg p-6 pb-0">Technologies</h1>
                 <div className="flex flex-wrap pb-6">
                     {technologies.map((technology, index) => {
                         return (
                             <div key={index} className='flex flex-col px-6'>
-                                <h3 className=''>{technology.title}:</h3>
-                                <ul className='list-disc list-inside'>
+                                <h3 className='pt-4'>{technology.title}:</h3>
+                                <ul className='flex flex-wrap gap-3 cursor-pointer'>
                                     {technology.technologies.map((tech, index) => {
+                                        let backgroundColor = getBackgroundByTech(tech); // Get background color by tech
+                                        if (technology.title == 'Hardware') backgroundColor = '#0f611a' // Deffault bg for hardware
                                         return (
-                                            <li key={index}>{tech}</li>
+                                            <li
+                                                style={backgroundColor ? { backgroundColor } : {}}
+                                                className={`${backgroundColor ? '' : ' bg-caribbean-current dark:bg-caribbean-current-dark'} text-white font-medium py-0.5 px-2`} // Default Background color (if no match)
+                                                key={index}
+                                            >
+                                                {tech}
+                                            </li>
                                         );
                                     })}
                                 </ul>
@@ -38,6 +123,6 @@ export default ({ title, paragraph, technologies, footnote, footnoteLink, footno
             </div>
             <p className="flex gap-1.5">{footnote}<span className='underline text-caribbean-current dark:text-caribbean-current-dark hover:text-orange-peel dark:hover:text-orange-peel-dark'>{footnoteLink}</span></p>
             <p className="flex gap-1.5">{footnoteTwo}<span className='underline text-caribbean-current dark:text-caribbean-current-dark hover:text-orange-peel dark:hover:text-orange-peel-dark'>{footnoteLinkTwo}</span></p>
-        </div>
+        </div >
     );
 };
