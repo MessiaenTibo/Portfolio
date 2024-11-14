438b334e62aa817b1b3a367d314d1097f11c9f83
TiboMessiaen <tibo.messiaen@student.howest.be>
2024-11-14 20:09:08 +0100

    Update: changed red color in darkmode to a bit darker red

--- Portfolio/tailwind.config.cjs
+++ Portfolio/tailwind.config.cjs
@@ -49,7 +49,7 @@ module.exports = {
         dark: '#02020A',
         'anti-flash-white-dark': '#08121A',
         'orange-peel-dark': '#FFCF2D   ', // '#FFCF2D' OR'#4FFF42' OR '#CC7B00'
-        'caribbean-current-dark': '#FF2233', // '#0E424F' OR '#FF2233'
+        'caribbean-current-dark': '#cc2233', // '#0E424F' OR '#FF2233'
         'federal-blue-dark': '#04011A',
       },
       screens: {
