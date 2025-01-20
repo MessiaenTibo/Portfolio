32be564041506814b3f7e15999b381fcdd0b6805
TiboMessiaen <tibo.messiaen@student.howest.be>
2025-01-20 11:16:27 +0100

    Fix: Homescreen was to big on laptop screen. Did not properly fit the screen

--- Portfolio/src/routes/Home.tsx
+++ Portfolio/src/routes/Home.tsx
@@ -20,15 +20,15 @@ function Home() {
             <div className=''>
                 <Title title="Welcome to my portfolio!" typeAnimation={true} animationDelay={80} />
 
-                <p className='pb-2 text-sm max-w-lg m-auto xl:p-0 xl:text-base xl:max-w-xl xl:pb-3 2xl:text-xl 2xl:max-w-2xl 2xl:pb-5'>
+                <p className='pb-2 text-sm max-w-lg m-auto xl:p-0 xl:text-base xl:max-w-xl xl:pb-3 3xl:text-xl 3xl:max-w-2xl 3xl:pb-5'>
                     Hey, I am Tibo Messiaen, a <b>Full Stack Web & App Developer</b> from Belgium. I have a <b>Bachelor's degree in Multimedia and Creative Technologies</b>. I am a hardworking individual and always eager to learn new things.
                 </p>
 
-                <p className='pb-4 text-sm max-w-lg m-auto xl:p-0 xl:text-base xl:max-w-xl xl:pb-6 2xl:text-xl 2xl:max-w-2xl 2xl:pb-8'>
+                <p className='pb-4 text-sm max-w-lg m-auto xl:p-0 xl:text-base xl:max-w-xl xl:pb-6 3xl:text-xl 3xl:max-w-2xl 3xl:pb-8'>
                     This portfolio website is designed to highlight my skills as a full-stack developer and to provide insights into who I am as an individual. It serves as a platform to showcase the projects I have worked on, the technologies I am proficient in, and the creative solutions I bring to the table.
                 </p>
 
-                <a href={cv} target="_blank" className='flex overflow-visible gap-2 px-5 py-2  bg-caribbean-current dark:bg-caribbean-current-dark w-fit text-white rounded mb-8 group/Resume hover:text-orange-peel dark:hover:text-orange-peel-dark'>
+                <a href={cv} target="_blank" className='flex overflow-visible gap-2 px-5 py-2  bg-caribbean-current dark:bg-caribbean-current-dark w-fit text-white rounded mb-6 3xl:mb-8 group/Resume hover:text-orange-peel dark:hover:text-orange-peel-dark'>
                     <p className=''>Resume</p>
                     {/* <Download className='' /> */}
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-download overflow-visible">
@@ -40,7 +40,7 @@ function Home() {
                     </svg>
                 </a>
 
-                <div className='flex gap-2 mb-8'>
+                <div className='flex gap-2 mb-6 3xl:mb-8'>
                     <a href="/learning" className='flex flex-col justify-center p-2 w-full max-w-[200px] aspect-[3/2] bg-white dark:bg-dark text-center shadow-md hover:bg-orange-peel dark:hover:bg-orange-peel-dark group/experience'>
                         <div className='flex justify-center items-center text-caribbean-current dark:text-caribbean-current-dark font-bold text-4xl group-hover/experience:text-white'>4+</div>
                         <div className=' font-medium group-hover/experience:text-white'>Years of programming experience</div>
@@ -52,7 +52,7 @@ function Home() {
                 </div>
 
 
-                <div className='flex justify-evenly bg-caribbean-current dark:bg-caribbean-current-dark text-white py-3 mb-8'>
+                <div className='flex justify-evenly bg-caribbean-current dark:bg-caribbean-current-dark text-white py-3 mb-6 3xl:mb-8'>
                     <a href='https://github.com/MessiaenTibo' className='w-full group/Phone' target='_blank'>
                         <Github className='stroke-white group-hover/Phone:stroke-orange-peel dark:group-hover/Phone:stroke-orange-peel-dark   w-12 h-12 m-auto' />
                         <p className='group-hover/Phone:text-orange-peel dark:group-hover/Phone:text-orange-peel-dark m-auto w-fit'>Github</p>
